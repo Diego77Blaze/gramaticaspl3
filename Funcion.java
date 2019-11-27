f46e831236308d4b3ce8769ccfa6b2ecc4aa65fa
JavierBrRuiz <javier.brenes@edu.uah.es>
2019-11-27 11:25:28 +0100

    Subido Funcion

---
+++ Funcion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Funcion{
+
+    private int nParametros;
+    private HashMap<String, String> variables;
+    private HashMap<String, LlamadaFuncion> llamadasFuncion; //parametros y veces que se utiliza (o numero de parametros) quiza puntuacion
+    private HashMap<Integer, String> operadores;
+}
