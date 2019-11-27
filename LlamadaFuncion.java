ca190615d9683799305ec5ee0a743a746271889b
JavierBrRuiz <javier.brenes@edu.uah.es>
2019-11-27 12:01:29 +0100

    Añadidos Tipo y atributos a funciones

---
+++ LlamadaFuncion.java
@@ -0,0 +1,13 @@
+
+
+public class LlamadaFuncion{
+
+    private String nombre;
+    private int parametros;
+    private int repeticiones;
+    private int puntuacion = 0;
+
+    public LlamadaFuncion(){
+        this.puntuacion = 2;
+    }
+}
---
+++ Tipo.java
@@ -0,0 +1,6 @@
+public class Tipo{
+
+    private String numero = "numero";
+    private String cadena = "cadena";
+    private String t_void = "void";
+}
